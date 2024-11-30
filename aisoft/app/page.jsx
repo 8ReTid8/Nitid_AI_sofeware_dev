6c3eb87e3ea339079a364d028ab73f008726d78c
8ReTid8 <125667954+8ReTid8@users.noreply.github.com>
2024-11-30 15:54:28 +0700

    y

---
+++ aisoft/app/page.jsx
@@ -0,0 +1,10 @@
+export default function HomePage() {
+    return (
+      <div className='mx-auto flex justify-center flex-col gap-5 bg-base-100 max-w-screen-xl px-32'>
+    
+        <div className="divider text-2xl font-bold">End soon</div>
+        <div className="divider text-2xl font-bold">All Products</div>
+      </div >
+  
+    )
+  }
